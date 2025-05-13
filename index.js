3f496432ad2dcba74f5c8e505eddc060168f4cca
Ferrankhalid21 <kevinkhalid21@gmail.com>
2025-05-13 12:27:49 +0300

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tablinks = document.getElementsByClassName("tab-cotent");
+
+function opentab(tabname){
+    for(tablinks of tablinks){
+        tablinks.ClassList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.ClassList.remove("active-tab")
+    }
+}
